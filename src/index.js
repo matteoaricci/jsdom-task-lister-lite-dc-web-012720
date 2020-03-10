29d89e9c2801ce3d5cd890ee68dc05fd93143df0
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-10 15:32:00 -0400

    Commiting

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+   getForm().addEventListener("submit", processFormSubmit)
 });
+
+function getForm() {
+  return document.querySelector('form')
+}
+
+function processFormSubmit(event) {
+  
+  event.preventDefault()
+  let newDescript = event.target.children[1].value
+  let newPrio = event.target.children[3].value
+  event.target.reset()
+  return makeNewLi(newDescript, newPrio)
+  
+  
+}
+
+function makeNewLi(task, otherTask) {
+
+  let target = document.getElementById('tasks')
+  let newLi = document.createElement('li')
+
+  newLi.innerText = task
+  newLi.className = otherTask
+  let idCount = document.getElementById('tasks').childElementCount
+  newLi.id = `todos-list-${idCount + 1}`
+
+
+  if (newLi.className == "high") {
+    target.insertBefore(newLi, target.childNodes[0])
+  } else if (newLi.className == "medium") {
+    target.insertBefore(newLi, target.child )
+  } else {
+    target.appendChild(newLi)
+  }
+
+  // target.appendChild(newLi)
+  
+}
